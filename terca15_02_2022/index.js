b9c6535774bb353a5e771a8ca11461d8cda7a302
Pablo Paixão <zkyouta@hotmail.com>
2022-02-15 22:14:10 -0300

    Feat: Barra de busca

---
+++ terca15_02_2022/index.js
@@ -0,0 +1,48 @@
+const palavrasChave = [
+  'STREAM OBS',
+  'stream obs delay',
+  'stream',
+  'obs',
+  'delay',
+  'pia area de serviço',
+  'pia',
+  'meu nome',
+  'nome',
+];
+
+const inputBusca = document.querySelector('#inputBusca');
+
+function showBarraDeBusca(e) {
+  const barraDeBusca = document.querySelector('#busca');
+  if (!barraDeBusca) return;
+
+  const { value: texto } = e.target;
+
+  // Se tiver acima de 1 caracter ele poe classe, se não remove
+  if (texto.length == 0) {
+    barraDeBusca.classList.remove('visivel');
+    return;
+  }
+
+  const palavrasAchadas = palavrasChave
+    .map((palavra) => {
+      const formatPalavra = palavra.toLowerCase();
+      const formatTexto = texto.toLowerCase();
+      if (!formatPalavra.includes(formatTexto)) return;
+      return `<li>${palavra}</li>`;
+    })
+    .filter((_) => _)
+    .join('');
+
+  if (!palavrasAchadas) {
+    barraDeBusca.innerHTML =
+      'Não foram encontradas palavras para este resultado';
+    return;
+  }
+
+  barraDeBusca.classList.add('visivel');
+
+  barraDeBusca.innerHTML = palavrasAchadas;
+}
+
+inputBusca && inputBusca.addEventListener('input', showBarraDeBusca);
